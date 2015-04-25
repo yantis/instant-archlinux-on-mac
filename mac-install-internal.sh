60efab30e8028d8d72979f0f7d56107bbfac972f
Jonathan Yantis <devnull@yantis.net>
2015-04-25 12:34:34 -0700

    mac-install-internal.sh

--- mac-install-internal.sh
+++ mac-install-internal.sh
@@ -12,7 +12,7 @@
 # Exit on any error whatsoever
 # since we don't actually modify the physical drive until the very end
 ###############################################################################
-set -e -u
+set -e -u -o pipefail
 
 ###############################################################################
 # Get the model of this Mac/Macbook
@@ -37,19 +37,13 @@ wget -O /root/initial_configuration.sh \
 mkdir /arch
 unsquashfs -d /squashfs-root /root/airootfs.sfs
 mount -o loop /squashfs-root/airootfs.img /arch
-
-# mount -t proc none /arch/proc
-# mount -t sysfs none /arch/sys
-# mount -o bind /dev /arch/dev
+mount -t proc none /arch/proc
+mount -t sysfs none /arch/sys
+mount -o bind /dev /arch/dev
 
 # Important for pacman (for signature check)
 # (Doesn't seem to matter at all they are still messed up.)
-# mount --bind /dev/pts /arch/dev/pts
-
-mount --bind /proc /arch/proc
-mount --bind /sys /arch/sys
-mount --bind /dev /arch/dev
-mount --bind /dev/pts /arch/dev/pts
+mount -o bind /dev/pts /arch/dev/pts
 
 ###############################################################################
 # Use Google's nameservers though I believe we may be able to simply copy the
@@ -67,6 +61,7 @@ chroot /arch haveged
 ###############################################################################
 # Init pacman
 ###############################################################################
+cp /etc/pacman.d/gnupg /arch/etc/pacman.d/gnupg
 chroot /arch  pacman-key --init
 chroot /arch  pacman-key --populate archlinux
 
