9c0605df2756331ae9ffbd99aaae6c7d571d51b8
Jonathan Yantis <devnull@yantis.net>
2015-04-26 13:20:38 -0700

    Tools renamed:    ../mount-efi.sh -> mount-efi.sh new file:   remove-rEFInd.sh renamed:    ../uninstall.sh -> uninstall.sh

---
+++ tools/mount-efi.sh
@@ -0,0 +1,8 @@
+# Mount the EFI Volume
+
+#!/bin/bash
+if [ ! -d  /Volumes/ESP ]; then
+  echo "Mounting EFI volume"
+  sudo mkdir -p /Volumes/ESP
+  sudo mount -t msdos /dev/disk0s1 /Volumes/ESP
+fi
--- tools/remove-rEFInd.sh
+++ tools/remove-rEFInd.sh
@@ -1,6 +1,9 @@
+# Remove rEFInd 
+
 #!/bin/bash
 if [ ! -d  /Volumes/ESP ]; then
   echo "Mounting EFI volume"
   sudo mkdir -p /Volumes/ESP
   sudo mount -t msdos /dev/disk0s1 /Volumes/ESP
 fi
+sudo rm -r /Volumes/ESP/EFI/refind
