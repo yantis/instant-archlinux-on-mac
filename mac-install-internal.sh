400df2652644b013a2bdcf1f0af2ba1fc717b8cf
Jonathan Yantis <devnull@yantis.net>
2015-04-26 08:42:31 -0700

    mac-install-internal.sh

--- mac-install-internal.sh
+++ mac-install-internal.sh
@@ -103,8 +103,8 @@ echo "SigLevel = Never" >> /arch/etc/pacman.conf
 
 chroot /arch pacman-key -r 962DDE58 --keyserver hkp://subkeys.pgp.net
 chroot /arch pacman-key --lsign 962DDE58
-# chroot /arch pacman-key -r AE6866C7962DDE58 --keyserver hkp://subkeys.pgp.net
-# chroot /arch pacman-key --lsign AE6866C7962DDE58
+chroot /arch pacman-key -r AE6866C7962DDE58 --keyserver hkp://subkeys.pgp.net
+chroot /arch pacman-key --lsign AE6866C7962DDE58
 
 ###############################################################################
 # Allow for colored output in pacman.conf
@@ -682,10 +682,16 @@ chroot /arch chown -R user:users /home/user/
 # We already did this above but for some reason we need to do it now or it
 # there will be db issues with pacman when ran.
 ###############################################################################
-chroot /arch pacman-key -r 962DDE58 --keyserver hkp://subkeys.pgp.net
+# chroot /arch pacman-key -r 962DDE58 --keyserver hkp://subkeys.pgp.net
 chroot /arch pacman-key --lsign 962DDE58
-chroot /arch pacman-key -r AE6866C7962DDE58 --keyserver hkp://subkeys.pgp.net
+# chroot /arch pacman-key -r AE6866C7962DDE58 --keyserver hkp://subkeys.pgp.net
 chroot /arch pacman-key --lsign AE6866C7962DDE58
+
+if grep -i -A1 "AMD" /systeminfo | grep -qi "GPU" ; then
+  # chroot /arch pacman-key -r 653C3094 --keyserver hkp://subkeys.pgp.net
+  chroot /arch pacman-key --lsign 653C3094
+fi
+
 
 ###############################################################################
 # Force root user to change password on next login.
