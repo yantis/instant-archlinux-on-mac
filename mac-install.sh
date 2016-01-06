1a3c0f287ebb380cb52dd088369820cdc1e36394
Jonathan Yantis <devnull@yantis.net>
2016-01-05 16:01:12 -0800

    mac-install.sh

--- mac-install.sh
+++ mac-install.sh
@@ -345,7 +345,8 @@ else
 
   else
     # (cd refind-bin-0.8.7 && sudo sh install.sh --alldrivers)
-    (cd refind-bin-0.10.1 && sudo sh refind-install --alldrivers)
+    # (cd refind-bin-0.10.1 && sudo sh refind-install --alldrivers)
+    (cd refind-bin-0.10.1 && sudo sh refind-install)
   fi
 
   rm -r refind-bin-0.10.1
