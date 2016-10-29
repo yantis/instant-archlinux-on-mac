79abfd80f62b927e9d0d71f1a4182ed122d6b639
Jonathan Yantis <yantis@yantis.net>
2016-10-29 04:59:31 -0700

    Update mac-install-internal.sh

--- mac-install-internal.sh
+++ mac-install-internal.sh
@@ -162,7 +162,7 @@ cp /var/cache/pacman/custom/* /arch/var/cache/pacman/custom/
 ###############################################################################
 # echo "** Syncing pacman database **"
 ###############################################################################
-# chroot /arch pacman -Syy --noconfirm
+chroot /arch pacman -Syy --noconfirm
 
 ###############################################################################
 # Have pacman use aria2 for downloads and give it extreme patience
@@ -171,15 +171,12 @@ cp /var/cache/pacman/custom/* /arch/var/cache/pacman/custom/
 # chroot /arch bash -c "(cd /var/cache/pacman/general && yes | pacman --noconfirm -U sqlite* aria2* c-ares*)"
 # echo "XferCommand = /usr/bin/printf 'Downloading ' && echo %u | awk -F/ '{printf \$NF}' && printf '...' && /usr/bin/aria2c -m0 -q --allow-overwrite=true -c --file-allocation=falloc --log-level=error --max-connection-per-server=2 --max-file-not-found=99 --min-split-size=5M --no-conf --remote-time=true --summary-interval=0 -t600 -d / -o %o %u && echo ' Complete!'" >> /etc/pacman.conf
 
-#echo "Removing imagemagick as causing conflicts"
-#chroot /arch pacman -R --noconfirm imagemagick
-
 ###############################################################################
-#echo "Installing cached general packages"
+echo "Installing cached general packages"
 ###############################################################################
 #chroot /arch pacman --noconfirm -R vim-minimal
 # chroot /arch pacman --noconfirm -R vim
-#chroot /arch yes | pacman --noconfirm --needed -U /var/cache/pacman/general/*.pkg.tar.xz
+chroot /arch pacman --noconfirm --needed -U /var/cache/pacman/general/yaour*.pkg.tar.xz
 
 ###############################################################################
 # update after pushing packages from docker container to get the system 
@@ -188,8 +185,6 @@ cp /var/cache/pacman/custom/* /arch/var/cache/pacman/custom/
 
 echo "** Updating System **"
 chroot /arch pacman -Syyu --noconfirm
-chroot /arch pacman --noconfirm --needed -S yaourt
-
 ###############################################################################
 # Setup Infinality Fonts
 # Moved to DOCKERFILE
